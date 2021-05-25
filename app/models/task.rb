ac66876b56502a8ef7b2f262a9bec49c5982db45
AdrianIlaga <adrianilaga771@gmail.com>
2021-05-25 19:50:19 +0800

    Finished categories  model and controller TDD

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ApplicationRecord
+    belongs_to :category
 
     validates :title, presence: true,
                       uniqueness: true
 
     validates :priority, presence: true, inclusion: { in: (1..5), message: "Must be an integer between 1 to 5" }
 
-    validates :category_id, presence: true
+    # validates :category_id, presence: true
 
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,64 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  test "should get categories index" do 
+    get categories_path 
+    assert_response :success
+  end
+
+  test "should get new category page" do
+    get new_category_path
+    assert_response :success
+  end
+
+  test "should create new user and redirect" do
+    assert_difference -> {Category.count} do
+      post create_category_path, params: { category: {title: 'Testing', 
+      description: 'Tasks related to testing'} }
+    end
+    assert_response :redirect
+  end
+
+  test "should get existing category" do
+    category = dummy_category
+    get show_category_path(category)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    category = dummy_category
+
+    get edit_category_path(category)
+    assert_response :success
+  end
+
+  test "should get update" do
+    category = dummy_category
+    patch update_category_path(category), params: { category: {title: 'Testing', 
+      description: 'Tasks related to testing'} }
+    assert_response :redirect
+  end
+
+  test "should delete" do
+    category = dummy_category
+    assert_difference -> {Category.count}, -1 do
+      delete delete_category_path(category)
+    end
+    assert_response :redirect
+  end
+
+  # test "should get delete" do
   # end
+  
+  private
+
+  def dummy_category
+    category = Category.new
+    category.title = "Test"
+    category.description = "Test description"
+    category.save
+    return category
+  end
+
+
 end
