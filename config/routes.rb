2feaae5b383ef965f9edc91e42b9842ea8913ed3
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-03 13:32:15 +0800

    Removed unecessary routes, removed tasks show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-#   get '/categories' => 'categories#index', as: 'categories'
-#   get '/categories/new' => 'categories#new', as: 'new_category'
-#   post '/categories/new' => 'categories#create', as: 'create_category'
-#   get '/categories/:id' => 'categories#show', as: 'show_category'
-#   get '/categories/:id/edit' => 'categories#edit', as: 'edit_category'
-#   patch '/categories/:id/edit' => 'categories#update', as: 'update_category'
-#   delete '/categories/:id' => 'categories#destroy', as: 'delete_category'
-  # put '/categories/:id/edit' => 'categories#update', as: 'edit_category'
   
   resources :categories do
-    resources :tasks , :except => [:index]
+    resources :tasks , :except => [:index, :show]
   end
+
 end
