5d1ee7fa77601107088a8e8e2d3eda99be7a2d40
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-07 13:13:01 +0800

    added tests to User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "user must have username" do
+    user = User.new
+    # user.username = @user.username
+    user.email = @user.email
+    user.encrypted_password = @user.encrypted_password
+    assert_not user.save, "Saved without username"
+  end
+
+  test "user must have email" do
+    user = User.new
+    user.username = @user.username
+    # user.email = @user.email
+    user.encrypted_password = @user.encrypted_password
+    assert_not user.save, "Saved without email"
+  end
+
+  test "user must have password" do 
+    user = User.new
+    user.username = @user.username
+    user.email = @user.email
+    # user.encrypted_password = @user.encrypted_password
+    assert_not user.save, "Saved without password"
+  end
+
+
 end
